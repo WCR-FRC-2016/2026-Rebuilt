3d98eb39fff534c215d370f53a9978ddd4c75e49
WCRJ 5492 <frc5492@gmail.com>
2026-02-05 20:58:26 -0500

    collector setup

--- src/main/java/frc/robot/subsystems/collector/CollectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/collector/CollectorSubsystem.java
@@ -1,32 +1,67 @@
 package frc.robot.subsystems.collector;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CollectorSubsystem extends SubsystemBase {
     private final SparkMax collectorWheels;
-    private final SparkMax collectorPivot;
+    private final SparkMax collectorPivotM;
+    private final SparkMax collectorPivotS;
     private static int collectorWheelsCanId = -1;
-    private static int collectorPivotCanId = -1;
+    private static int collectorPivotMCanId = -1;
+    private static int collectorPivotSCanId = -1;
     private static double collectPower = -1;
     private static double uncollectPower = -1;
-    PIDController pid = new PIDController (0, 0, 0);
-    public CollectorSubsystem(){
-        collectorWheels = new SparkMax(collectorWheelsCanId,MotorType.kBrushed);
-        collectorPivot = new SparkMax(collectorPivotCanId,MotorType.kBrushless);
+
+    PIDController pid = new PIDController(0, 0, 0);
+
+    public CollectorSubsystem() {
+        collectorWheels = new SparkMax(collectorWheelsCanId, MotorType.kBrushed);
+        collectorPivotM = new SparkMax(collectorPivotMCanId, MotorType.kBrushless);
+        collectorPivotS = new SparkMax(collectorPivotSCanId, MotorType.kBrushless);
+
+        
+          SparkMaxConfig globalConfig = new SparkMaxConfig();
+          SparkMaxConfig collectorPivotMConfig = new SparkMaxConfig();
+          SparkMaxConfig collectorPivotSConfig = new SparkMaxConfig();
+          
+          
+          globalConfig
+          .smartCurrentLimit(50)
+          .idleMode(IdleMode.kBrake);
+          
+          collectorPivotMConfig
+          .apply(globalConfig)
+          .inverted(true);
+          
+          collectorPivotSConfig
+          .apply(globalConfig)
+          .follow(collectorPivotM);
+          
+          collectorPivotM.configure(collectorPivotMConfig, ResetMode.kResetSafeParameters,
+          PersistMode.kPersistParameters);
+          collectorPivotS.configure(collectorPivotSConfig, ResetMode.kResetSafeParameters,
+          PersistMode.kPersistParameters);
+         
     }
-    public void collect(){
+
+    public void collect() {
         collectorWheels.set(collectPower);
     }
 
-    public void uncollect(){
+    public void uncollect() {
         collectorWheels.set(uncollectPower);
     }
 
-    public void collectorPivot(){
-        
+    public void collectorPivot() {
+        // has to pivot more than 90 degrees
+
     }
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -1,5 +1,31 @@
 package frc.robot.subsystems.shooter;
 
-public class ShooterSubsystem {
-    
-}
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    private final SparkMax shooterWheelsM;
+    private final SparkMax shooterWheelsS;
+    private final SparkMax shooterPivot;
+    private static int shooterWheelsMCanId = -1;
+    private static int shooterWheelsSCanId = -1;
+    private static int shooterPivotCanId = -1;
+    private static double shooterPower = -1;
+    PIDController pid = new PIDController (0,0,0);
+    public ShooterSubsystem(){
+        shooterWheelsM = new SparkMax(shooterWheelsMCanId,MotorType.kBrushed);
+        shooterWheelsS = new SparkMax(shooterWheelsSCanId,MotorType.kBrushed);
+        shooterPivot = new SparkMax(shooterPivotCanId,MotorType.kBrushless);
+    }
+    public void shoot(){
+      //  shooterWheels.set(shooterPower);
+    }
+
+    public void shooterPivot(){
+
+    }
+    }
+
